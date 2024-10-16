c0529dea2220e2d904ce12e3cf192f522d9dac8e
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-16 23:28:09 +0530

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -1,19 +1,19 @@
 export const skillsData = [
   "Node JS",
-  'Javascript',
-  'AWS',
-  "GCP", "Azure", 
-  'MongoDB',
-  'Git',
-  'Docker',
-  'Figma',
-  'Nginx',
-  "Python"
-]
+  "Javascript",
+  "AWS",
+  "GCP",
+  "Azure",
+  "MongoDB",
+  "Git",
+  "Docker",
+  "Figma",
+  "Nginx",
+  "Python",
+];
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
-
 
 // AVAILABLE SKILLS
 
