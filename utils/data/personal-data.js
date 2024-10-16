170f9f4c97923cb5f66c8ce4a2ce85d6b1a4c350
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-16 23:28:01 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,18 +1,20 @@
 export const personalData = {
   name: "Aviral",
   firstname: "Aviral",
-  profile: '/profile.png',
+  profile: "/profile.png",
   designation: "Backend and DevOps engineer",
-  description: "My name is Aviral Srivastava. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript.",
-  email: 'aviralji4@gmail.com',
-  phone: '+91',
-  address: 'Lucknow, UP, India',
-  github: 'https://github.com/godofgeeks23',
-  facebook: '',
-  linkedIn: 'https://www.linkedin.com/in/aviralsrivastav23/',
-  twitter: 'https://x.com/godofgeeks_',
-  stackOverflow: 'https://stackoverflow.com/users/13393219/godofgeeks',
+  description:
+    "My name is Aviral Srivastava. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript.",
+  email: "aviralji4@gmail.com",
+  phone: "+91",
+  address: "Lucknow, UP, India",
+  github: "https://github.com/godofgeeks23",
+  facebook: "",
+  linkedIn: "https://www.linkedin.com/in/aviralsrivastav23/",
+  twitter: "https://x.com/godofgeeks_",
+  stackOverflow: "https://stackoverflow.com/users/13393219/godofgeeks",
   leetcode: "",
   devUsername: "godofgeeks",
-  resume: "https://drive.google.com/drive/folders/1vZfVwiT_n0awiL3UR6SBfC4t65uUFuh0?usp=sharing"
-}
+  resume:
+    "https://drive.google.com/drive/folders/1vZfVwiT_n0awiL3UR6SBfC4t65uUFuh0?usp=sharing",
+};
