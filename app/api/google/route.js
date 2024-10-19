349673d1ed671f82f6c08e4f35240d59d6ff9d40
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-19 21:35:24 +0530

    Configured Next app for cloudfare next-on-pages

--- app/api/google/route.js
+++ app/api/google/route.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
+export const runtime = 'edge';
+
 export async function POST(request) {
   const reqBody = await request.json();
   const secret_key = process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY;
