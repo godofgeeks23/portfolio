0230696d935e36c90bf3ee1d5e566b80eae3d388
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-10-21 12:25:27 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   profile: "/profile.png",
   designation: "Backend and DevOps engineer",
   description:
-    "My name is Aviral Srivastava. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript.",
+    "Hello, I'm Aviral — a young developer from India with a keen interest in Backend systems, Distributed and Cloud computing, Security, and DevOps. I enjoy tackling challenging engineering problems and devising innovative solutions. Learning new technologies, especially those related to web development, brings me great pleasure. JavaScript is my preferred language for turning ideas into reality.",
   email: "aviralji4@gmail.com",
   phone: "+91",
   address: "Lucknow, UP, India",
